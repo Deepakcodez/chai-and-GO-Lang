3fa2d073d360b52c810c5870ee06c80157f7fffb
Deepak <deepak2603dk@gmail.com>
2024-03-14 00:51:18 +0530

    if else in go

---
+++ conditional/ifelse.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main()  {
+	
+	num1 := 2;
+	num2 := 4;
+
+	if num1 > num2 {
+		fmt.Println("num1 > num2")
+	}else if num1 < num2 {
+		fmt.Println("num1 < num2");
+	}else{
+		fmt.Println("Something went wrong")
+	}
+
+
+
+
+}
