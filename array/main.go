3bf3bc1cfc3afd2253c6ee06cf5ed9cfd932fb36
Deepak <deepak2603dk@gmail.com>
2024-03-23 00:10:44 +0530

    modify array file

--- array/main.go
+++ array/main.go
@@ -4,14 +4,14 @@ import "fmt"
 
 func main() {
 
-	fruits := [3]string{"peach", "mango"}  // way 1
+	
 	var names [3]string                    // way 2
 
 	names[0] = "Deepak"
 	names[1] = "Musu"
 	names[2] = "Abhi"
 
-	fmt.Println("fruit list is: ", fruits)
-	fmt.Println("lenght of fruit list is: ", len(fruits))
+
+	fmt.Println("lenght of name list is: ", len(names))
 	fmt.Println(" Name list is: ", names)
 }
